ce8ef4ae921c1fbce3a153a042709e7a8a2a5cff
Andrii <greenzelenetskyi@gmail.com>
2019-04-21 20:00:19 +0300

    Add db connection

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const chatroomEndpoints = require('./routes/chatroomRoutes');
+const { logMessage } = require('./utils/util.js');
 
 const app = express();
+const MESSAGES_ROUTE = '/api/messages';
 
-app.listen(process.env.PORT || 8080, () => console.log(`listens to port ${process.env.PORT}`));
+(async function connectDb() {
+  const client = await MongoClient.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true });
+  app.locals.db = client.db();
+}());
+
+app.use(bodyParser.json());
+app.use(MESSAGES_ROUTE, chatroomEndpoints.router);
+
+app.listen(process.env.PORT || 8080, () => logMessage(`listens to port ${process.env.PORT}`));
